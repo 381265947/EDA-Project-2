d66ed75b753d9b7f3d59fc6be27dc46ac1e17e10
381265947 <trashitautho@yahoo.com>
2014-09-17 15:59:21 -0400

    Made plot 6 bigger.

--- plot6.R
+++ plot6.R
@@ -3,7 +3,7 @@
 # motor vehicle sources in Los Angeles County, California (fips == "06037"). 
 # Which city has seen greater changes over time in motor vehicle emissions?
 
-#setwd("C:/M/coursera/exploratory data analysis/project 2")
+setwd("C:/M/coursera/exploratory data analysis/project 2")
 
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
@@ -25,5 +25,5 @@ lines(years, tpmeBA, type = "b", col = cols[1])
 lines(years, tpmeLA, type = "b", col = cols[2])
 legend('topright', c("Baltimore", "Los Angeles"), lty = 1, col = cols, bty = "n")
 
-dev.copy(png, "plot6.png", width=5, height=5, units="in", res=100)
+dev.copy(png, "plot6.png", width=7, height=7, units="in", res=100)
 dev.off()
